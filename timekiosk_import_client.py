b74615788f0c5c8b959be66a09893ee2308ab70d
fredrls <fredrls@ifi.uio.no>
2014-01-29 00:19:55 +0100

    added file

---
+++ timekiosk_import_client.py
@@ -0,0 +1,14 @@
+#
+# timekiosk_import_client.py
+#
+# @author Fredrik Stave <fredrls@ifi.uio.no>
+# @version 1.0
+# @since 29.01.2014
+#
+# Copyright (C) 2012 Fredrik Stave
+# THIS PROGRAM COMES WITH ABSOLUTELY NO WARRANTY
+# THIS IS FREE SOFTWARE, AND YOU ARE WELCOME TO REDISTRIBUTE IT
+# UNDER CERTAIN CONDITIONS; SEE 'LICENCE' FOR DETAILS OR
+# SEE <http://heim.ifi.uio.no/fredrls/license/gnu-gpl/LICENSE>
+#
+
